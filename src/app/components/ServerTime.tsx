b95ae28f6f96b0b533b151a774984e7d4b893131
Ryan Canty <rpc@amazon.com>
2023-08-05 21:29:30 -0400

    Add SSR: - Get server time - Render in Amplify UI Alert

--- src/app/components/ServerTime.tsx
+++ src/app/components/ServerTime.tsx
@@ -5,5 +5,7 @@ async function getServerTime() {
 export default async function ServerTime() {
   const time = await getServerTime();
 
+  console.log("Time:", new Date(time).toLocaleString());
+
   return <>Current time on server: {new Date(time).toLocaleString()}</>;
 }
